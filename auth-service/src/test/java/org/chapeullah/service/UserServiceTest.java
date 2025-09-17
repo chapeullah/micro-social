f1c6ceef43f2f6fa4d9098d18c908ab9aec73cdc
chapeullah <chapeullah@gmail.com>
2025-09-17 13:32:32 +0300

    Update files

--- auth-service/src/test/java/org/chapeullah/service/UserServiceTest.java
+++ auth-service/src/test/java/org/chapeullah/service/UserServiceTest.java
@@ -1,13 +1,34 @@
 package org.chapeullah.service;
 
+import org.chapeullah.exception.DuplicateUserException;
+import org.chapeullah.infrastructure.kafka.EventsProducer;
+import org.chapeullah.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
+    @Mock UserRepository userRepository;
+    @Mock PasswordEncoder passwordEncoder;
+    @Mock JwtService jwtService;
+    @Mock EventsProducer eventsProducer;
+
+    @InjectMocks UserService userService;
+
     @Test
-    void sampleTest() {
-        int a = 2 + 3;
-        
+    void register_duplicateEmail_throwsException() {
+        when(userRepository.existsByEmail("some@email.com")).thenReturn(true);
+        assertThrows(DuplicateUserException.class, () -> userService.register("some@email.com", "qwerty123456"));
+        verifyNoInteractions(passwordEncoder, jwtService, eventsProducer);
     }
 
 }
